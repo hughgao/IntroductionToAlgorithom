5f5946dcce7fc730a745f9db5dc36b6aee3fb9e0
huigao <hammerqin@hotmail.com>
2018-04-16 17:01:37 -0700

    check in the brute force method

--- DivideConquer/bruteforceMaxSubArray/bruteforceMaxSubArray.py
+++ DivideConquer/bruteforceMaxSubArray/bruteforceMaxSubArray.py
@@ -1,21 +1,21 @@
 import sys
-from typing import Tuple
+from typing import Tuple, List
 
 
-def brute_force_max_sub_array(the_array):
+def brute_force_max_sub_array(the_array: List):
 
     maximum = -sys.maxsize - 1
-    the_result: Tuple[int, int, int] = tuple(0, 0, maximum)
+    the_result: Tuple[int, int, int] = (0, 0, maximum)
     for i in range(len(the_array)-1):
         total = the_array[i];
         if total > maximum:
             maximum = total
-            the_result = tuple(i, i, maximum)
+            the_result = (i, i, maximum)
         for j in range(i+1, len(the_array)):
             total = total + the_array[j]
             if total > maximum:
                 maximum = total
-                the_result = tuple(i, j, maximum)
+                the_result = (i, j, maximum)
     return the_result
 
 
