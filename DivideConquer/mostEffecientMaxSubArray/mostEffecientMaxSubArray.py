872de29dd9d9e9696e218624ee57ccbae36b424e
huigao <hammerqin@hotmail.com>
2018-04-18 17:30:18 -0700

    more checkin

--- DivideConquer/mostEffecientMaxSubArray/mostEffecientMaxSubArray.py
+++ DivideConquer/mostEffecientMaxSubArray/mostEffecientMaxSubArray.py
@@ -3,17 +3,17 @@ from typing import Tuple,List
 '''the complexity is o(n)'''
 
 
-def max_sub_array(input: List)-> Tuple[int, int, int]:
-    max_so_far: int = input[0]
-    total: int = input[0]
+def max_sub_array(the_input: List)-> Tuple[int, int, int]:
+    max_so_far: int = the_input[0]
+    total: int = the_input[0]
     start_index: int = 0
     end_index: int = 0
-    for index in range(1, len(input)):
+    for index in range(1, len(the_input)):
         if total < 0:
             start_index = index
-            total = input[index]
+            total = the_input[index]
         else:
-            total = total + input[index]
+            total = total + the_input[index]
             if total > max_so_far:
                 max_so_far = total
                 end_index = index
